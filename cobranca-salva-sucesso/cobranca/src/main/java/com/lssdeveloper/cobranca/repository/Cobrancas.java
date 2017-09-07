be53479f8847574fec1921ff0b0da67a741e986b
Leandro Serra <leandro.serra.10@gmail.com>
2017-09-07 08:52:36 -0300

    Inserindo projeto cobranca-lssd (corrente)

--- cobranca-salva-sucesso/cobranca/src/main/java/com/lssdeveloper/cobranca/repository/Cobrancas.java
+++
@@ -1,9 +0,0 @@
-package com.lssdeveloper.cobranca.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.lssdeveloper.cobranca.model.Cobranca;
-
-public interface Cobrancas extends JpaRepository<Cobranca, Long> {
-
-}
